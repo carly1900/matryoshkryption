7ac25bfdf1b1a232586f7f26df7a380c8048ab0d
Lena David <lena.david@inpt.fr>
2018-02-28 18:05:42 +0100

    Refactor angecrypt.py

--- src/angecrypt.py
+++ src/angecrypt.py
@@ -2,7 +2,7 @@
 
 # AngeCryption: getting valid files after encryption
 
-# takes any file as input, and a standard PDF/PNG/JPG/FLV as target
+# takes any file as input, and a standard PDF/PNG/JPG/FLV/MP3 as target
 # will create a result_file that is source_file with appended 'garbage'
 # and once ENcrypted with the chosen algorithm with the supplied script, it will show target_file
 
@@ -12,9 +12,16 @@
 
 # Port to Python 3 and mp3 support by L. David
 
+import argparse
 import binascii
+import logging
 import struct
 import sys
+
+# Logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level="DEBUG", format="%(asctime)s:%(name)s:%(lineno)s:%(levelname)s - %(message)s")
+
 
 PNGSIG = b"\x89PNG\r\n\x1a\n"
 JPGSIG = b"\xff\xd8"
@@ -22,156 +29,163 @@ FLVSIG = b"FLV"
 MP3SIG = b"\xff\xfb"
 ID3SIG = b"ID3"
 
-filetype = ""   # useful only to name file when decrypted
-
-source_file, target_file, result_file, encryption_key, algo = sys.argv[1:6]
-
-if algo.lower() == "aes":
-    from Crypto.Cipher import AES
-
-    algo = AES
-    BLOCKSIZE = 16
-else:
-    from Crypto.Cipher import DES3  # will work only with JPEG as others require 16 bytes block size
-
-    algo = DES3
-    BLOCKSIZE = 8
+BLOCKSIZE = 16
 
 
 def pad(pt):
     return pt + (BLOCKSIZE - len(pt) % BLOCKSIZE) * bytes([BLOCKSIZE - len(pt) % BLOCKSIZE])  # non-standard padding might be preferred for PDF
 
 
-# from Crypto import Random
-# key = Random.new().read(16)
-key = encryption_key.encode()
+def compute_blocksize(algo):
+    global BLOCKSIZE
+    if algo.lower() == "aes":
+        from Crypto.Cipher import AES
 
-with open(source_file, "rb") as f:
-    s = pad(f.read())
+        algo = AES
+        BLOCKSIZE = 16
+    else:
+        from Crypto.Cipher import DES3  # will work only with JPEG as others require 16 bytes block size
 
-with open(target_file, "rb") as f:
-    t = pad(f.read())
+        algo = DES3
+        BLOCKSIZE = 8
 
-p = s[:BLOCKSIZE]  # our first plaintext block
-ecb_dec = algo.new(key, algo.MODE_ECB)
+    logger.debug(BLOCKSIZE)
+    return algo
 
-# we need to generate our first cipher block, depending on the target type
 
-if t.startswith(PNGSIG):  # PNG
-    assert BLOCKSIZE >= 16
-    size = len(s) - BLOCKSIZE
-    filetype = "png"
+def angecrypt(source_file, target_file, result_file, encryption_key, algo):
+    algo = compute_blocksize(algo)
 
-    # our dummy chunk type
-    # 4 letters, first letter should be lowercase to be ignored
-    chunktype = b"aaaa"
+    # from Crypto import Random
+    # key = Random.new().read(16)
+    key = encryption_key.encode()
 
-    # PNG signature, chunk size, our dummy chunk type
-    c = PNGSIG + struct.pack(">I", size) + chunktype
+    with open(source_file, "rb") as f:
+        s = pad(f.read())
 
-    c = ecb_dec.decrypt(c)
-    #    IV = "".join([chr(ord(c[i]) ^ ord(p[i])) for i in range(BS)]))
-    IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
-    cbc_enc = algo.new(key, algo.MODE_CBC, IV)
-    result = cbc_enc.encrypt(s)
+    with open(target_file, "rb") as f:
+        t = pad(f.read())
 
-    # write the CRC of the remaining of s at the end of our dummy block
-    result = result + struct.pack(">I", binascii.crc32(result[12:]) % 0x100000000)
-    # and append the actual data of t, skipping the sig
-    result = result + t[8:]
+    p = s[:BLOCKSIZE]  # our first plaintext block
+    ecb_dec = algo.new(key, algo.MODE_ECB)
 
-elif t.startswith(JPGSIG):  # JPG
-    assert BLOCKSIZE >= 2
-    filetype = "jpg"
+    # we need to generate our first cipher block, depending on the target type
 
-    size = len(s) - BLOCKSIZE  # we could make this shorter, but then could require padding again
+    if t.startswith(PNGSIG):  # PNG
+        assert BLOCKSIZE >= 16
+        size = len(s) - BLOCKSIZE
+        filetype = "png"
 
-    # JPEG Start of Image, COMment segment marker, segment size, padding
-    c = JPGSIG + b"\xFF\xFE" + struct.pack(">H", size) + b"\0" * 10
+        # our dummy chunk type
+        # 4 letters, first letter should be lowercase to be ignored
+        chunktype = b"aaaa"
 
-    c = ecb_dec.decrypt(c)
-    #    IV = "".join([chr(ord(c[i]) ^ ord(p[i])) for i in range(BS)]))
-    IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
-    cbc_enc = algo.new(key, algo.MODE_CBC, IV)
-    result = cbc_enc.encrypt(s)
+        # PNG signature, chunk size, our dummy chunk type
+        c = PNGSIG + struct.pack(">I", size) + chunktype
 
-    # and append the actual data of t, skipping the sig
-    result = result + t[2:]
+        c = ecb_dec.decrypt(c)
+        #    IV = "".join([chr(ord(c[i]) ^ ord(p[i])) for i in range(BS)]))
+        IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
+        cbc_enc = algo.new(key, algo.MODE_CBC, IV)
+        result = cbc_enc.encrypt(s)
 
-elif t.startswith(FLVSIG):  # FLV
-    assert BLOCKSIZE >= 9
-    size = len(s) - BLOCKSIZE  # we could make this shorter, but then could require padding again
-    filetype = "flv"
+        # write the CRC of the remaining of s at the end of our dummy block
+        result = result + struct.pack(">I", binascii.crc32(result[12:]) % 0x100000000)
+        # and append the actual data of t, skipping the sig
+        result = result + t[8:]
 
-    # reusing FLV's sig and type, data offset, padding
-    c = t[:5] + struct.pack(">I", size + 16) + b"\0" * 7
+    elif t.startswith(JPGSIG):  # JPG
+        assert BLOCKSIZE >= 2
+        filetype = "jpg"
 
-    c = ecb_dec.decrypt(c)
-    #    IV = "".join([chr(ord(c[i]) ^ ord(p[i])) for i in range(BS)]))
-    IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
-    cbc_enc = algo.new(key, algo.MODE_CBC, IV)
-    result = cbc_enc.encrypt(s)
+        size = len(s) - BLOCKSIZE  # we could make this shorter, but then could require padding again
 
-    # and append the actual data of t, skipping the sig
-    result = result + t[9:]
+        # JPEG Start of Image, COMment segment marker, segment size, padding
+        c = JPGSIG + b"\xFF\xFE" + struct.pack(">H", size) + b"\0" * 10
 
-elif t.startswith(MP3SIG):  # MP3
-    assert BLOCKSIZE >= 16
-    size = len(s) - BLOCKSIZE
+        c = ecb_dec.decrypt(c)
+        #    IV = "".join([chr(ord(c[i]) ^ ord(p[i])) for i in range(BS)]))
+        IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
+        cbc_enc = algo.new(key, algo.MODE_CBC, IV)
+        result = cbc_enc.encrypt(s)
 
-    filetype = "mp3"
-    c = t[:4] + 12 * b"\0"
-    c = ecb_dec.decrypt(c)
+        # and append the actual data of t, skipping the sig
+        result = result + t[2:]
 
-    IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
-    cbc_enc = algo.new(key, algo.MODE_CBC, IV)
-    result = cbc_enc.encrypt(s) + t[4:]
+    elif t.startswith(FLVSIG):  # FLV
+        assert BLOCKSIZE >= 9
+        size = len(s) - BLOCKSIZE  # we could make this shorter, but then could require padding again
+        filetype = "flv"
 
-elif t.startswith(ID3SIG):  # MP3 with metadata
-    assert BLOCKSIZE >= 16
-    size = len(s) - BLOCKSIZE
+        # reusing FLV's sig and type, data offset, padding
+        c = t[:5] + struct.pack(">I", size + 16) + b"\0" * 7
 
-    filetype = "mp3"
-    c = t[:4] + 5 * b"\0" + b"#TSSE" + 2 * b"\0"
-    c = ecb_dec.decrypt(c)
+        c = ecb_dec.decrypt(c)
+        #    IV = "".join([chr(ord(c[i]) ^ ord(p[i])) for i in range(BS)]))
+        IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
+        cbc_enc = algo.new(key, algo.MODE_CBC, IV)
+        result = cbc_enc.encrypt(s)
 
-    IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
-    cbc_enc = algo.new(key, algo.MODE_CBC, IV)
-    result = cbc_enc.encrypt(s) + t[17:]
+        # and append the actual data of t, skipping the sig
+        result = result + t[9:]
 
-elif t.find(b"%PDF-") > -1:
-    assert BLOCKSIZE >= 16
-    size = len(s) - BLOCKSIZE  # we take the whole first 16 bits
-    filetype = "pdf"
+    elif t.startswith(MP3SIG):  # MP3
+        assert BLOCKSIZE >= 16
+        size = len(s) - BLOCKSIZE
 
-    # truncated signature, dummy stream object start
-    c = b"%PDF-\0obj\nstream"
+        filetype = "mp3"
+        c = t[:4] + 12 * b"\0"
+        c = ecb_dec.decrypt(c)
 
-    c = ecb_dec.decrypt(c)
-    #    IV = "".join([chr(ord(c[i]) ^ ord(p[i])) for i in range(BS)]))
-    IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
-    cbc_enc = algo.new(key, algo.MODE_CBC, IV)
-    result = cbc_enc.encrypt(s)
+        IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
+        cbc_enc = algo.new(key, algo.MODE_CBC, IV)
+        result = cbc_enc.encrypt(s) + t[4:]
 
-    # close the dummy object and append the whole t
-    # (we don't know where the sig is, we can't skip anything)
-    result = result + b"\nendstream\nendobj\n" + t
+    elif t.startswith(ID3SIG):  # MP3 with metadata
+        assert BLOCKSIZE >= 16
+        size = len(s) - BLOCKSIZE
 
-else:
-    print("file type not supported")
-    sys.exit()
+        filetype = "mp3"
+        c = t[:4] + 5 * b"\0" + b"#TSSE" + 2 * b"\0"
+        c = ecb_dec.decrypt(c)
 
-# we have our result, key and IV
+        IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
+        cbc_enc = algo.new(key, algo.MODE_CBC, IV)
+        result = cbc_enc.encrypt(s) + t[17:]
 
-# generate the result file
-cbc_dec = algo.new(key, algo.MODE_CBC, IV)
-with open(result_file, "wb") as f:
-    f.write(cbc_dec.decrypt(pad(result)))
+    elif t.find(b"%PDF-") > -1:
+        assert BLOCKSIZE >= 16
+        size = len(s) - BLOCKSIZE  # we take the whole first 16 bits
+        filetype = "pdf"
 
-# generate the script
-with open("dec-" + target_file + ".py", "w") as ds:
-    ds.write(
-        """from Crypto.Cipher import %(algo)s
+        # truncated signature, dummy stream object start
+        c = b"%PDF-\0obj\nstream"
+
+        c = ecb_dec.decrypt(c)
+        #    IV = "".join([chr(ord(c[i]) ^ ord(p[i])) for i in range(BS)]))
+        IV = bytes([c[i] ^ p[i] for i in range(BLOCKSIZE)])
+        cbc_enc = algo.new(key, algo.MODE_CBC, IV)
+        result = cbc_enc.encrypt(s)
+
+        # close the dummy object and append the whole t
+        # (we don't know where the sig is, we can't skip anything)
+        result = result + b"\nendstream\nendobj\n" + t
+
+    else:
+        print("file type not supported")
+        sys.exit()
+
+    # we have our result, key and IV
+
+    # generate the result file
+    cbc_dec = algo.new(key, algo.MODE_CBC, IV)
+    with open(result_file, "wb") as f:
+        f.write(cbc_dec.decrypt(pad(result)))
+
+    # generate the script
+    with open("dec-" + target_file + ".py", "w") as ds:
+        ds.write(
+            """from Crypto.Cipher import %(algo)s
 
 algo = %(algo)s.new(%(key)s, %(algo)s.MODE_CBC, %(IV)s)
 with open("%(source)s", "rb") as f:
@@ -185,3 +199,21 @@ with open("dec-%(target)s.%(filetype)s", "wb") as f:
             'IV': IV,
             'source': result_file,
             'target': target_file})
+
+
+def main():
+    parser = argparse.ArgumentParser(description="AngeCryption: getting valid files after encryption")
+    parser.add_argument("--content", "-c", required=True, help="Content to embed")
+    parser.add_argument("--input", "-i", required=True, help="Input/target file")
+    parser.add_argument("--output", "-o", required=False, help="Output/resulting file")
+    parser.add_argument("--encryption_key", "-k", required=False, help="Encryption key")
+    parser.add_argument("--algo", "-a", required=False, default="aes", help="Algorithm to use for encryption")
+
+    args = parser.parse_args()
+    logger.debug(args)
+
+    angecrypt(args.content, args.input, args.output, args.encryption_key, args.algo)
+
+
+if __name__ == "__main__":
+    main()
